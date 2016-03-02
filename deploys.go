f70b024253e3e2ecdf4d5991545f5bb6cc141ebc
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-03-02 11:39:50 -0800

    don't ignore .well-known/ dir (user for android)

--- deploys.go
+++ deploys.go
@@ -222,7 +222,7 @@ func (deploy *Deploy) deployDir(dir string) (*Response, error) {
 				return err
 			}
 
-			if strings.HasPrefix(rel, ".") || strings.Contains(rel, "/.") || strings.HasPrefix(rel, "__MACOS") {
+			if ignoreFile(rel) {
 				return nil
 			}
 
@@ -402,4 +402,14 @@ func (deploy *Deploy) WaitForReady(timeout time.Duration) error {
 
 	err := <-done
 	return err
+}
+
+func ignoreFile(rel string) bool {
+	if strings.HasPrefix(rel, ".") || strings.Contains(rel, "/.") || strings.HasPrefix(rel, "__MACOS") {
+		if strings.HasPrefix(rel, ".well-known/") {
+			return false
+		}
+		return true
+	}
+	return false
 }
